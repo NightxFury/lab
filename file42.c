2c6b76f70a49de5ba507c82d0cededfb6849aaf9
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-12 23:06:46 +0530

    Add files via upload

---
+++ file42.c
@@ -0,0 +1,23 @@
+// W.A.P in c to find factorial using function
+
+//#include <stdio.h>
+//
+//long long factorial(int n) {
+//    if (n == 0 || n == 1)
+//        return 1;
+//    else
+//        return n * factorial(n - 1);
+//}
+//int main() {
+//    int n;
+//    printf("Enter a non-negative integer: ");
+//    scanf("%d",&n);
+//
+//    if ( n < 0)
+//        printf("Factorial is not defined for negative numbers.\n");
+//    else
+//        printf("Factorial of %d is %11d\n", n, factorial(n));
+//
+//    return 0;
+//
+//}
